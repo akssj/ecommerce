e78aab7ef4d8771c335cdb73b949858aace39b88
TotalyActiveGitUster <radek.czyzowicz@gmail.com>
2024-02-14 16:38:18 +0100

    filling site with products is now triggered after page is loaded, minor var refactor

--- src/main/resources/static/scriptUtility.js
+++ src/main/resources/static/scriptUtility.js
@@ -15,15 +15,18 @@ function showAccountOptions() {
     document.getElementById('loggedInContent').classList.remove('d-none');
 }
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const isLoggedIn = getCookie('loggedIn');
-    const loggedInDropdown = document.getElementById('loggedInContent');
-    if (isLoggedIn === 'true') {
-      showAccountOptions();
-    } else {
-      showLoginForm();
-    }
-  });
+/*===========================
+Website handling on page load
+=============================*/
+document.addEventListener('DOMContentLoaded', function () {
+  fillProducts();
+  const isLoggedIn = getCookie('loggedIn');
+  if (isLoggedIn === 'true') {
+    showAccountOptions();
+  } else {
+    showLoginForm();
+  }
+});
 
 function setCookie(name, value) {
     const expirationDate = new Date();
@@ -32,17 +35,17 @@ function setCookie(name, value) {
     document.cookie = cookieString;
 }
 
-function getCookie(cname) {
-  let name = cname + "=";
+function getCookie(cookie_name) {
+  let name = cookie_name + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
+  let cookieArray = decodedCookie.split(';');
+  for(let i = 0; i <cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) == ' ') {
+      cookie = cookie.substring(1);
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
+    if (cookie.indexOf(name) == 0) {
+      return cookie.substring(name.length, cookie.length);
     }
   }
   return "";
