61625e6394237cf14853bdb3e2088009c3359e44
TotalyActiveGitUster <radek.czyzowicz@gmail.com>
2024-02-20 18:03:42 +0100

    setting up better filter for requests

--- src/main/java/alledrogo/controller/AlledrogoController.java
+++ src/main/java/alledrogo/controller/AlledrogoController.java
@@ -2,7 +2,6 @@ package alledrogo.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
--- src/main/java/alledrogo/security/jwt/AuthTokenFilter.java
+++ src/main/java/alledrogo/security/jwt/AuthTokenFilter.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,31 +27,41 @@ public class AuthTokenFilter extends OncePerRequestFilter {
   private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
 
   @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-    String requestUri = request.getRequestURI();
-    if (requestUri.startsWith("/static/") || requestUri.startsWith("/images/")) {
-      response.setHeader("Cache-Control", "max-age=3600");
-      filterChain.doFilter(request, response);
-      return;
-    }
+  protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
     try {
       String token = parseJwt(request);
       if (token != null && jwtUtils.validateJwtToken(token)) {
         String username = jwtUtils.getUserNameFromJwtToken(token);
-
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
                 userDetails.getAuthorities());
         authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
         SecurityContextHolder.getContext().setAuthentication(authentication);
-      }else {
+      } else {
         logger.error("Invalid or missing JWT token");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or missing JWT token");
+        return;
       }
     } catch (Exception e) {
       logger.error("Cannot set user authentication: ", e);
+      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Cannot set user authentication: " + e.getMessage());
+      return;
     }
     filterChain.doFilter(request, response);
+  }
+
+  @Override
+  protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+    String path = request.getRequestURI();
+    return path.startsWith("/static") ||
+            path.startsWith("/images") ||
+            path.equals("/main") ||
+            path.matches("/category/\\w+") ||
+            path.equals("/auth/login") ||
+            path.equals("/auth/signup") ||
+            path.equals("/product/forSale") ||
+            path.equals("/product/categories") ||
+            path.matches("/product/\\w+/category");
   }
 
   private String parseJwt(HttpServletRequest request) {
